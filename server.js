bf7ef7014af6c056a7fffae450d6a27f79bb5b42
Sachin Ranjalkar <jarvis@Mac.domain.name>
2025-04-20 21:43:44 +0530

    Fix port for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public folder
 app.use(express.static("public"));
